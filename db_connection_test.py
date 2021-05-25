b9929561452c44f37ece8d62e83055f08a25b463
박준우 <pkjoho95@gmail.com>
2021-05-25 13:45:37 +0900

    implement client request from page

--- db_connection_test.py
+++ db_connection_test.py
@@ -8,5 +8,12 @@ class DBConnectionTest(unittest.TestCase):
 		result = db.get_restaurants()
 		print(result)
 
+	def test_get_restaurants(self):
+		db = DBConnection()
+		db.connect_to_db()
+		rest_id = 1
+		result = db.get_restaurant(rest_id)
+		print(result)
+
 if __name__ == '__main__':
     unittest.main()
