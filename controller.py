c3aba778d9bf6a70bfccabcd5244033f76c05c0c
박준우 <pkjoho95@gmail.com>
2021-05-30 00:25:28 +0900

    change category code to category name

--- controller.py
+++ controller.py
@@ -5,6 +5,8 @@ from model.restaurants import Restaurants, Parser
 from map_generator import MapGenerator
 from typing import List
 
+category_map = {0: "한식", 1: "중식", 2: "양식", 3:"일식", 4:"카페"}
+
 class Controller:
 	def __init__(self):
 		self.db = DBConnection()
@@ -15,7 +17,7 @@ class Controller:
 		search_list = Parser.to_list(self.db.get_restaurants())
 		self.db.close_connection()
 		curated_rests = Curator.curate_restaurant(search_list, request.args)
-		return self.page_mk.make_restaurants_list_page(curated_rests, rests)
+		return self.page_mk.make_restaurants_list_page(curated_rests, search_list)
 
 	def restaurant_controller(self, rest_id):
 		rest = self.db.get_restaurant(rest_id)
